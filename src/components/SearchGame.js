1ad2c765069d64e6f28879b8d4d32360475a71e9
PerryM123 <perrymiranda1@yahoo.com>
2018-04-14 03:39:28 +0900

    Changes to the json data and google maps prep

--- src/components/SearchGame.js
+++ src/components/SearchGame.js
@@ -1,46 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import loading_logo from './../images/loading_icon.png';
+
 
 class SearchGame extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      gamesList: ""
+      gamesList: "",
+      loading: true
     }
+    this.buttonHandler = this.buttonHandler.bind(this);
   }
   componentDidMount() {
     axios.get('gameData.json')
-     .then((results) => {
+      .then((results) => {
         const gamesList = results.data.games.map((data, num)=> {
-          // Trying to fix this
-          // Use this as reference: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
-          // There needs to be another router connected here.
-        const link = "/search-game/" + data.game_id;
-        return (
-          <li key={num}>
-            <Link to={link}>
-              <img src={data.game_img_thumbnail}/>
-              <p>{data.game_name}</p>
-            </Link>
-          </li>
-        );
+          const link = "/search-game/" + data.game_id;
+          this.setState({
+            loading: false
+          });
+          return (
+            <li key={num}>
+              <Link to={link}>
+                <img src={data.game_img_thumbnail}/>
+                <p>{data.game_name}</p>
+              </Link>
+            </li>
+          );
+        });
+        this.setState({
+          gamesList: gamesList
+        });
       });
-
-      this.setState({
-        gamesList: gamesList
-      });
-     });
-
+  }
+  buttonHandler() {
+    // load more items!!
   }
   render() {
     return (
       <div className="contents contents--search-game">
+        {
+          (this.state.loading) ? <div class="loading_now"><img src={loading_logo} alt="loading-icon" /></div> : null
+        }
         <h2>Search By Game</h2>
-        <ul>
+        <ul class="loaded-games">
           {this.state.gamesList}
         </ul>
-        <button>Load More</button>
+        <button onClick={this.buttonHandler}>Load More</button>
       </div>
     );
   }

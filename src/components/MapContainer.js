17f821d86cab2c890d2fd1e06698b43b30df35d9
PerryM123 <perrymiranda1@yahoo.com>
2018-05-09 11:26:22 +0900

    Testing Icons and Markers on the map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -8,9 +8,23 @@ export class MapContainer extends Component {
     this.state = {
       showingInfoWindow: false,
       activeMarker: {},
-      selectedPlace: {}
+      selectedPlace: {},
+      currentLocation: {}
     };
+    this.showCurrentPosition = this.showCurrentPosition.bind(this);
 
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    if (this.showCurrentPosition() ){
+      console.log("YES");
+    } else {
+
+      console.log("NO");
+    }
+  }
+  componentWillMount() {
+    console.log("componentWillMount");
   }
   onMarkerClick(props, marker, e) {
     this.setState({
@@ -19,22 +33,70 @@ export class MapContainer extends Component {
       showingInfoWindow: true
     });
   }
+  showCurrentPosition() {
+    console.log("showCurrentPosition now");
+    const map = this.map;
+    console.log("map: " + map);
+    const curr = this.state.currentLocation;
+    const google = this.props.google;
+    const maps = google.maps;
+
+    if (map) {
+        console.log("YES map time");
+        let center = new maps.LatLng(curr.lat, curr.long);
+        map.panTo(center);
+    } else {
+        console.log("NO map time");
+    }
+  }
   render() {
     const google=window.google
     if (!this.props.google) {
       return <div>Loading...</div>;
     }
 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          currentLocation: {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          }
+        });
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      alert("error");
+    }
+
+
     return (
       <div className="the-map">
-        <Map google={this.props.google} zoom={14}>
+        <button onClick={this.showCurrentPosition}> Hello </button>
+        <Map centerAroundCurrentLocation google={this.props.google} zoom={14}>
           <Marker
             onClick={this.onMarkerClick}
             icon={{
+              url: "/map-icons/tshirt.svg",
               anchor: new google.maps.Point(32, 32),
               scaledSize: new google.maps.Size(64, 64)
             }}
-            name={"Current location"}
+            name="Mikado"
+        position={{ lat: 35.7127351, lng: 139.7034548 }}
+        title="The marker`s title will appear as a tooltip."
+
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            icon={{
+              url: "/map-icons/shorts.svg",
+              anchor: new google.maps.Point(32, 32),
+              scaledSize: new google.maps.Size(64, 64)
+            }}
+            name="HEY! Arcade"
+        position={{ lat: 35.699024, lng: 139.771062 }}
+        title="The marker`s title will appear as a tooltip."
+
           />
           <InfoWindow
             marker={this.state.activeMarker}

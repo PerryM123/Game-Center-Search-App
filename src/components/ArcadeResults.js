9681784aa863c5c8fbbd52d0850428bf58a06718
PerryM123 <perrymiranda1@yahoo.com>
2018-05-10 15:05:16 +0900

    Added a React Question

--- src/components/ArcadeResults.js
+++ src/components/ArcadeResults.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 import Swiper from 'react-id-swiper';
 import './../scss/swiper.scss'; // Double check to see if this is the correct way to use swiper' scss
 
+/*
+* React Question:
+* So in this project, to avoid overloading the render function,
+* I move the logic out to a separate function. But wouldn't it also be possible to
+* make these into their own components?
+*
+* How do I make the decision between between:
+* 1) making another functions for the logic
+* and
+* 2) making a component to have that logic (maybe this leads into smart components)
+*
+*/
+
 class ArcadeResults extends Component {
   constructor(props) {
     super(props);
@@ -112,6 +125,7 @@ class ArcadeResults extends Component {
       </div>
     )
   }
+
 
   render() {
     const ourGame = this.props.match.params.id; // game in parameter

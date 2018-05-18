4acded4834a591450b246cf9243c98a9f39795c6
PerryM123 <perrymiranda1@yahoo.com>
2018-05-18 20:49:39 +0900

    Added a header and footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ class App extends Component {
   render() {
     return (
       <div>
+        <header className="contents contents--header">
+          <a href="/" className="someLogo">Logo</a>
+          <ul>
+            <li><a href="#">Find Game</a></li>
+            <li><a href="#">Find Arcade</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </header>
         <Main />
+        <footer className="contents contents--footer">
+          <p>This is a footer</p>
+          <a href="/">This is a footer LOGO</a>
+        </footer>
       </div>
     );
   }
